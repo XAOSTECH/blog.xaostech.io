199b20985b8e4c5d0a23429eeb12df589a8d76d0
xaoscience <69734795+xaoscience@users.noreply.github.com>
2026-04-01 22:11:40 +0000

    fix: use getSecurityHeaders + direct mutation in middleware
    applySecurityHeaders fallback creates new Response(res.body, ...) locking
    the body stream on CF Workers. Use direct res.headers.set() mutation instead.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { defineMiddleware, sequence } from 'astro:middleware';
-import { applySecurityHeaders } from '../shared/types/security';
+import { getSecurityHeaders } from '../shared/types/security';
 
 const sessionMiddleware = defineMiddleware(async (context, next) => {
   const { cookies, locals } = context;
@@ -45,7 +45,11 @@ const sessionMiddleware = defineMiddleware(async (context, next) => {
 
 const securityMiddleware = defineMiddleware(async (_context, next) => {
   const res = await next();
-  return applySecurityHeaders(res);
+  const sec = getSecurityHeaders();
+  for (const k of Object.keys(sec)) {
+    res.headers.set(k, sec[k]);
+  }
+  return res;
 });
 
 export const onRequest = sequence(sessionMiddleware, securityMiddleware);
